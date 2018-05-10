47e140edb1a6c39c41957a1965b4e7758b290224
Daniil Cherednik <dan.cherednik@gmail.com>
2018-05-10 21:08:22 +0000

    Use optimized for real input/output FFT. Approx. 40% faster.

--- src/dsd/fftresample.rs
+++ src/dsd/fftresample.rs
@@ -1,10 +1,10 @@
 extern crate num;
 extern crate rustfft;
+extern crate rfft;
 
-use self::rustfft::algorithm::Radix4;
 use self::num::complex::Complex;
-use self::rustfft::FFT;
 use self::rustfft::num_traits::Zero;
+use self::rfft::{RFFT, RFFTImpl, RIFFT, RIFFTImpl};
 
 use std::f64::consts::PI;
 use std::boxed::Box;
@@ -14,10 +14,10 @@ pub struct ResampleCtx {
     oversample: usize,
     input_half_win: Vec<f64>,
     output_half_win: Vec<f64>,
-    input_buffer: Vec<Complex<f64>>,
+    input_buffer: Vec<f64>,
     output_buffer: Vec<f64>,
-    fft: Box<FFT<f64>>,
-    ifft: Box<FFT<f64>>
+    fft: Box<RFFT<f64>>,
+    ifft: Box<RIFFT<f64>>
 }
 
 pub trait Resampler {
@@ -31,7 +31,7 @@ fn create_half_win (sz: usize, scale: bool) -> Vec<f64> {
     // sine window
     let s: f64;
     if scale {
-        s = (sz * 2) as f64;
+        s = sz as f64;
     } else {
         s = 1f64;
     }
@@ -49,8 +49,8 @@ impl Resampler for ResampleCtx {
                       output_half_win: create_half_win(sz * oversample, false),
                       input_buffer: vec![Zero::zero(); sz * 2],
                       output_buffer: vec![Zero::zero(); sz * oversample],
-                      fft: Box::new(Radix4::new(sz * 2, false)),
-                      ifft: Box::new(Radix4::new((sz * oversample * 2), true))
+                      fft: Box::new(RFFTImpl::new(sz * 2)),
+                      ifft: Box::new(RIFFTImpl::new(sz * oversample * 2))
         }
     }
 
@@ -63,46 +63,40 @@ impl Resampler for ResampleCtx {
 
         // Apply window and copy input to the second part of buffer
         for i in 0..input_end {
-            self.input_buffer[buf_midle + i].re = input[i] * self.input_half_win[input_end - 1 - i];
-            self.input_buffer[buf_midle + i].im = Zero::zero();
+            self.input_buffer[buf_midle + i] = input[i] * self.input_half_win[input_end - 1 - i];
         }
 
-
-        let mut spectrum: Vec<Complex<f64>> = vec![Zero::zero(); self.frame_size * 2];
+        let mut spectrum: Vec<Complex<f64>> = vec![Zero::zero(); self.frame_size];
         self.fft.process(&mut self.input_buffer, &mut spectrum);
 
         // Apply window and copy to the first part of buffer (overlap)
         for i in 0..input.len() {
-            self.input_buffer[i].re = input[i] * self.input_half_win[i];
-            self.input_buffer[i].im = Zero::zero();
+            self.input_buffer[i] = input[i] * self.input_half_win[i];
         }
 
-        let mut new_spectrum: Vec<Complex<f64>> = Vec::with_capacity(self.frame_size * 2 * self.oversample);
-        let middle = spectrum.len() / 2;
+        let mut new_spectrum: Vec<Complex<f64>> = Vec::with_capacity(self.frame_size * self.oversample);
+        let end = spectrum.len();
 
         // Create new spectrum
-        for i in 0..middle {
+        for i in 0..end {
             new_spectrum.push(spectrum[i]);
         }
 
-        for _j in 0..((self.frame_size * (self.oversample - 1))* 2) {
+        for _j in 0..(self.frame_size * (self.oversample - 1)) {
             new_spectrum.push(Complex::new(0f64, 0f64));
         }
 
-        new_spectrum[self.frame_size * self.oversample] = spectrum[middle];
-        spectrum[middle] = Complex::new(0f64, 0f64);
+        //Do we realy need this
+        new_spectrum[self.frame_size * self.oversample - 1] = spectrum[end - 1];
+        new_spectrum[end - 1] = Complex::new(0f64, 0f64);
 
-        for i in middle..spectrum.len() {
-            new_spectrum.push(spectrum[i]);
-        }
-
-        let mut result:   Vec<Complex<f64>> = vec![Zero::zero(); (self.frame_size * self.oversample * 2)];
+        let mut result: Vec<f64> = vec![Zero::zero(); (self.frame_size * self.oversample * 2)];
         self.ifft.process(&mut new_spectrum, &mut result);
 
         let half = result.len()/2;
         for i in 0..half {
-            oversample_buf[i] = result[i].re * self.output_half_win[i] + self.output_buffer[i];
-            self.output_buffer[i] = result[half + i].re * self.output_half_win[half - 1 - i];
+            oversample_buf[i] = result[i] * self.output_half_win[i] + self.output_buffer[i];
+            self.output_buffer[i] = result[half + i] * self.output_half_win[half - 1 - i];
         }
 
         return oversample_buf;
